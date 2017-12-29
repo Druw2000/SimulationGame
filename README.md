a9138b829615e219dd52dbc1c9207825580e1bb6
Druw2000 <aearnhardt2000@gmail.com>
2017-12-29 00:49:36 -0500

    Added build script, Tetris reference example, updated README, created manifest for jar, and organized source and class files into '/src' and '/bin', respectively

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SimulationGame
+Some useful commands:
+
+    git clone https://github.com/druw2000/SimulationGame
+    git add *
+    git commit -m "Added local files"
+    git push
+    javac -d ../build *.java
+    java -jar SimulationGame.jar
+    jar cfmv ../SimulationGame.jar ../manifest.txt *.class
--- src/Buttons.java
+++ src/Buttons.java
@@ -1,15 +1,18 @@
 
 public class Buttons {
     public void newGame() {
-        
+        System.out.println("New game");
     }
     public void loadGame() {
-        
+        System.out.println("Load game");
     }
     public void changeSettings() {
-        
+        System.out.println("Open settings");
     }
     public void showAbout() {
-        
+        System.out.println("Show about");
+    }
+    public void quit() {
+        System.out.println("Quit");
     }
 }
